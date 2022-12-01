b9cdc768798e96f8664f508b2f8f1dec5c85ed08
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-12-01 11:43:54 +0100

    Update MyVector.h

--- Lab3/MyVector.h
+++ Lab3/MyVector.h
@@ -7,31 +7,42 @@ class MyVector{
 
 public:
 
+    /* costruttori */
     MyVector();
     MyVector(unsigned int size);
+    /* PER UNA CLASSE VECTOR PIU' COMPLESSA, il terzo lab non vuole implementare std::vector completamente
     MyVector(std::initializer_list<double> list);
     MyVector(const MyVector& v);
     MyVector(MyVector&& v);
+    */
+    /* distruttore */
     ~MyVector() {delete[] ptr;}
     
-    int getSize() const {return size;}
+    /* get functions */
     int getCapacity() const {return capacity;}
     bool isEmpty() const {return (capacity == 0?"SI":"NO");} 
     
+    /* funzioni di accesso sicuro */
     double safeGet(const int index);
     void safeSet(const int index, const double elem);
     
+    /* operator[] */
     double& operator[](int index) {return ptr[index];}
     double operator[](int index) const {return ptr[index];}
+    /*
     MyVector& operator=(const MyVector& v);
     MyVector& operator=(MyVector&& v);
+    */
     
+    /*
     void pushBack(const double& value);
     void popBack();
     void reserve(unsigned int capacity);
     MyVector at(unsigned int index);
     const MyVector& at(unsigned int index) const;
+    */
 
+    /* stampa del vettore */
     void printVector();
     
     class Invalid{};
@@ -39,7 +50,6 @@ public:
 private:
 
     unsigned int capacity;
-    unsigned int size;
     double* ptr; 
     
 };
