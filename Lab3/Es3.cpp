ac19dd426b9307e66fe6d717894d9722ee8e52d8
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-12-01 11:33:10 +0100

    Update Es3.cpp

--- Lab3/Es3.cpp
+++ Lab3/Es3.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
 void printeReference(const int& i, const double& d){
-    
     std::cout<<"intero : "<<i<<std::endl
              <<"double : "<<d<<std::endl;
 }
 
 void printPointer(int* i, double* d){
-    
     std::cout<<"intero : "<<i<<std::endl
              <<"double : "<<d<<std::endl;
 }
 
 int main(){
-    
     int i = 2;
     double d = 2.2;
     std::cout<<"Passaggio per const reference"<<std::endl;
