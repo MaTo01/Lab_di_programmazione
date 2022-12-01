cd1fa7b217d451aca1c4dcd69496b04519b41d7f
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2022-12-01 11:29:32 +0100

    Update Fibonacci.cpp

--- FibonacciComplete/Fibonacci.cpp
+++ FibonacciComplete/Fibonacci.cpp
@@ -7,29 +7,20 @@ void print(vector<int> v, string s){
 	cout << s << "{ ";
 	for(int i = 0; i < v.size(); ++i){
 		cout << v[i];
-		if(i != v.size()-1){
-			cout << ", ";
-		}
+		if(i != v.size()-1) {cout << ", ";}
 	} 
 	cout << " }";
 }
 
 vector<int> fibonacciOverKill(int x, int y, vector<int> v, int n){
-	
-	if(v.size() != 0){
-		v.clear();
-	}
+	if(v.size() != 0) {v.clear();}
 	
 	if(x + y < 0 || x < 0 || y < 0){
 		throw invalid_argument("I dati inseriti non sono validi\n");
 	}
-	
 	v.push_back(x);
 	v.push_back(y);
-	
-	for(int i = 2; i < n; ++i){
-		v.push_back(v[i-2] + v[i-1]);
-	}
+	for(int i = 2; i < n; ++i) {v.push_back(v[i-2] + v[i-1]);}
 	return v;
 }
 
@@ -47,9 +38,8 @@ int main()
 	cin >> n;
 	cout << "\n";
 	vector<int> v;
-	try{
-		v = fibonacciOverKill(x,y,v,n);
-	}catch(invalid_argument& e){
+	try {v = fibonacciOverKill(x,y,v,n);}
+	catch(invalid_argument& e){
 		cerr<< e.what() << "I primi numeri inseriti devono essere positivi!"<< endl;
 		return -1;
 	}
